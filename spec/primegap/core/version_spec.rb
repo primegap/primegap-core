b13d207ae5c3042d757c20cd125f249520fe789d
Luca Tironi <luca.tironi@gmail.com>
2015-11-08 19:11:03 +0100

    Improve gem specs

--- spec/primegap/core/version_spec.rb
+++ spec/primegap/core/version_spec.rb
@@ -1,7 +1,6 @@
-require 'spec_helper.rb'
-require 'primegap/core/version'
+require 'spec_helper'
 
-RSpec.describe Primegap::Core do
+RSpec.describe Primegap::Core, type: :model do
   describe 'VERSION' do
     it { expect(described_class.const_defined?(:VERSION)).to eq true }
     it { expect(described_class::VERSION).to be_kind_of(String) }
