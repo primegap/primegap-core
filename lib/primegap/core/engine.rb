0c5af5b6e77a73d784bbd726485562d2c0d41aab
Luca Tironi <luca.tironi@gmail.com>
2015-11-08 19:40:09 +0100

    Change engine_name to primegap_core

--- lib/primegap/core/engine.rb
+++ lib/primegap/core/engine.rb
@@ -1,6 +1,8 @@
 module Primegap
   module Core
     class Engine < ::Rails::Engine
+      engine_name 'primegap_core'
+
       config.generators do |g|
         g.test_framework :rspec, fixture: false
         g.assets false
